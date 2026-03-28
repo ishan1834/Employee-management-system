1c63a8582fa381eb51e4807ef33528951139aa30
vivek rai <vivekrai18th@gmail.com>
2026-03-28 10:30:00 +0530

    Remove UseAttendanceDataProps interface
    Removed the UseAttendanceDataProps interface definition.

--- useAttendanceData.ts
+++ useAttendanceData.ts
@@ -3,9 +3,9 @@ import { format, startOfMonth, endOfMonth } from 'date-fns';
 import { supabase } from '@/integrations/supabase/client';
 import { AttendanceRecord } from './types';
 
-interface UseAttendanceDataProps {
-  adminProfile: any;
-  isSuperAdmin: boolean;
-  selectedDate: Date;
-  selectedMonth: Date;
-}
+export const useAttendanceData = ({
+  adminProfile,
+  isSuperAdmin,
+  selectedDate,
+  selectedMonth,
+}: UseAttendanceDataProps) => {
