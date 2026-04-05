85367fb71466e90a7b28c295b489d64fe3813dd6
vivek rai <vivekrai18th@gmail.com>
2026-04-05 23:20:57 +0530

    updating some important parts of this file

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -42,3 +42,22 @@ function useSidebar() {
   }
   return context
 }
+type SidebarContext = {
+  state: "expanded" | "collapsed"
+  open: boolean
+  setOpen: (open: boolean) => void
+  openMobile: boolean
+  setOpenMobile: (open: boolean) => void
+  isMobile: boolean
+  toggleSidebar: () => void
+}
+
+const SidebarContext = React.createContext<SidebarContext | null>(null)
+
+function useSidebar() {
+  const context = React.useContext(SidebarContext)
+  if (!context) {
+    throw new Error("useSidebar must be used within a SidebarProvider.")
+  }
+  return context
+}
