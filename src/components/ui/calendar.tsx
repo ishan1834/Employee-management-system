296af64aa08c103d69e2c717790c46b5a250b84a
vivek rai <vivekrai18th@gmail.com>
2026-04-12 01:04:26 +0530

    day picker setup ui enhancement

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -12,3 +12,7 @@ function Calendar({
   showOutsideDays = true,
   ...props
 }: CalendarProps) {
+  return (
+    <DayPicker
+      showOutsideDays={showOutsideDays}
+      className={cn("p-3", className)}
