1c09f707b3d51f7248814cff275808e99197f7ed
vivek rai <vivekrai18th@gmail.com>
2026-04-12 01:06:26 +0530

    final cahnges in ui-vivek

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -2,10 +2,11 @@ import * as React from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { DayPicker } from "react-day-picker";
 
-
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
+
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
+
 function Calendar({
   className,
   classNames,
@@ -16,8 +17,7 @@ function Calendar({
     <DayPicker
       showOutsideDays={showOutsideDays}
       className={cn("p-3", className)}
-
-            classNames={{
+      classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
@@ -51,7 +51,7 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-            components={{
+      components={{
         IconLeft: ({ ..._props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ..._props }) => <ChevronRight className="h-4 w-4" />,
       }}
