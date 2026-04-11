cde02d20e8eb5bb3f43cfadc5b5e14a590377f1f
vivek rai <vivekrai18th@gmail.com>
2026-04-12 01:03:27 +0530

    Defines props type based on the user event

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -5,3 +5,4 @@ import { DayPicker } from "react-day-picker";
 
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
+export type CalendarProps = React.ComponentProps<typeof DayPicker>;
