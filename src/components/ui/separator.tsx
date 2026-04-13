ae7cae3c77ae2362948b8459eda17eba52f289c8
Rashmi Jha <749rashmijha@gmail.com>
2026-04-13 07:17:10 +0530

    Refactor Separator component for improved styling
    Updated separator component to use new class names for sizing.

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -3,27 +3,28 @@ import * as SeparatorPrimitive from "@radix-ui/react-separator"
 
 import { cn } from "@/lib/utils"
 
+/**
+ * A visual or semantic separator between content.
+ * Supports both horizontal and vertical orientations.
+ */
 const Separator = React.forwardRef<
   React.ElementRef<typeof SeparatorPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
->(
-  (
-    { className, orientation = "horizontal", decorative = true, ...props },
-    ref
-  ) => (
-    <SeparatorPrimitive.Root
-      ref={ref}
-      decorative={decorative}
-      orientation={orientation}
-      className={cn(
-        "shrink-0 bg-border",
-        orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
-        className
-      )}
-      {...props}
-    />
-  )
-)
+>(({ className, orientation = "horizontal", decorative = true, ...props }, ref) => (
+  <SeparatorPrimitive.Root
+    ref={ref}
+    decorative={decorative}
+    orientation={orientation}
+    className={cn(
+      "shrink-0 bg-border",
+      // Conditional sizing based on orientation
+      orientation === "horizontal" ? "h-px w-full" : "h-full w-px",
+      className
+    )}
+    {...props}
+  />
+))
+
 Separator.displayName = SeparatorPrimitive.Root.displayName
 
 export { Separator }
